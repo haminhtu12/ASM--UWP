0468a11547e5be2ea17f820c652e0ba140e1481c
Tu Ha <54472431+haminhtu12@users.noreply.github.com>
2020-08-27 13:54:52 +0700

    first commit

---
+++ ASM_HA_MINH_TU/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ASM_HA_MINH_TU
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ApplicationDataContainer settings;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void toggleSwitch2_Toggled(object sender, RoutedEventArgs e)
+        {
+            // create a new toggle switch item from sender object
+            ToggleSwitch toggleSwitch2 = sender as ToggleSwitch;
+
+            // if the toggle switch exists
+            if (toggleSwitch2 != null)
+            {
+                // if toggle switch is on, change to light theme
+                if (toggleSwitch2.IsOn == true)
+                {
+                    rootPivot.Background = new SolidColorBrush(Colors.DarkGray);
+                    stackpanel.Background = new SolidColorBrush(Colors.DarkGray);
+                    stackpanel2.Background = new SolidColorBrush(Colors.DarkGray);
+                    stackpanel3.Background = new SolidColorBrush(Colors.DarkGray);
+                    stackpanel4.Background = new SolidColorBrush(Colors.DarkGray);
+
+                }
+                // if not, change to dark theme
+                else
+                {
+                    rootPivot.Background = new SolidColorBrush(Colors.MidnightBlue);
+                    stackpanel.Background = new SolidColorBrush(Colors.MidnightBlue);
+                    stackpanel2.Background = new SolidColorBrush(Colors.MidnightBlue);
+                    stackpanel3.Background = new SolidColorBrush(Colors.MidnightBlue);
+                    stackpanel4.Background = new SolidColorBrush(Colors.MidnightBlue);
+                }
+
+
+            }
+        }
+
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchKeyword_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+
+        }
+    }
+}
